a59066bb5f64cd67ee7f8c574c9e5e033208d2fa
codyc1515 <50791984+codyc1515@users.noreply.github.com>
2025-08-13 15:55:22 +1200

    fix: resolve merge conflict

--- custom_components/em6/api.py
+++ custom_components/em6/api.py
@@ -14,16 +14,16 @@ class em6Api:
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36 Edg/111.0.1661.54"
         }
         response = requests.get(self._url_base + "region/price", headers=headers)
-        
+
         if response.status_code == requests.codes.ok:
             data = response.json()
             if not data:
                 _LOGGER.warning('Fetched prices successfully, but did not find any')
-                
+
             if data['items']:
                 for location in data['items']:
                     if location['grid_zone_name'] == self._location:
                         return location
         else:
             _LOGGER.error('Failed to fetch prices')
-            return data
+        return None
